6f84213d040decc5f7fc0525930d16726ce01368
Leila <lailarhoni96@gmail.com>
2023-02-02 21:22:04 +0100

    modifications

--- src/main/java/com/projet/sponsoring/Controller/PosteController.java
+++ src/main/java/com/projet/sponsoring/Controller/PosteController.java
@@ -4,26 +4,22 @@ import com.projet.sponsoring.Model.Entreprise;
 import com.projet.sponsoring.Model.Poste;
 import com.projet.sponsoring.Service.EntrepriseService;
 import com.projet.sponsoring.Service.PosteService;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public class PosteController {
     @Autowired
     public PosteService posteservice;
     @Autowired
     public EntrepriseService entrepriseService;
-
-    @RequestMapping(value="/updatePoste",method = RequestMethod.POST)
-    @ResponseBody
-    public ModelAndView updatePoste(@RequestParam Integer id, String contenu){
-        //Poste p =posteservice.findbyid(id);
-        //posteservice.updateposte(p);
-        posteservice.updatePoste(id,contenu);
+    @PostMapping(value="/createPoste")
+    public ModelAndView createPoste( @RequestParam("file") String contenu, MultipartFile file ){
+        posteservice.createPoste(contenu, file);
         return new ModelAndView("club_home");
     }
     @DeleteMapping("/delete{id}")
@@ -31,16 +27,24 @@ public class PosteController {
         return posteservice.deletePoste(id);
     }
 
+    @RequestMapping(value="/updatePoste",method = RequestMethod.POST)
+    @ResponseBody
+    public ModelAndView updatePoste(@RequestParam Integer id, String contenu){
+        posteservice.updatePoste(id,contenu);
+        return new ModelAndView("club_home");
+    }
+
     @RequestMapping ("/listePostes")
     public List<Poste> getAllPostes(){
         return posteservice.listAllPostes();
     }
 
-    @RequestMapping(value="/approuverPoste",method = RequestMethod.POST)
+    @PutMapping(value="/approuverPoste")
     @ResponseBody
     public ModelAndView approuverPoste(@RequestParam Integer idposte,String name){
-        Poste p =posteservice.findbyid(idposte);
         Entreprise e = entrepriseService.findbyname(name);
+        posteservice.approuverPoste(idposte,e.getId_organisme());
         return new ModelAndView("Entreprise_home");
     }
+
 }
