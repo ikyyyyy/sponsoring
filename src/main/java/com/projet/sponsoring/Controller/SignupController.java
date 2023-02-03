eb4d79caf0051e2cca4744817af9ad6c8999e1b2
ikyyyyy <ferdikram1@gmail.com>
2023-02-03 12:44:07 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/projet/sponsoring/Controller/LoginController.java
    #	src/main/java/com/projet/sponsoring/Controller/PosteController.java
    #	src/main/java/com/projet/sponsoring/Service/LoginService.java
    #	src/main/java/com/projet/sponsoring/Service/PosteService.java

---
+++ src/main/java/com/projet/sponsoring/Controller/SignupController.java
@@ -0,0 +1,15 @@
+package com.projet.sponsoring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SignupController {
+
+    /*@GetMapping("/signup1")
+    public String signup() {
+        return "SignupClub";
+    }*/
+}
